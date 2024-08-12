b3444b8d36625de927d7f8bb564a38bbe13ce2f7
Swierszczykowaty <loolok1232@gmail.com>
2024-08-12 12:02:37 +0200

    [ADD] - Choose class duel

--- TestHeros.cs
+++ TestHeros.cs
@@ -10,51 +10,77 @@ namespace RPG_warrior_expanded
     {
         static void Main(string[] args)
         {
-            int win1 = 0; //class test
-            int win2 = 0;
-            int win3 = 0;
-            int goalwin = 10000;
-            
+            int winPlayer = 0;
+            int winOpponent = 0;
+            int goalwin = 10;
+            string playerChoice;
 
-            Warrior warrior1;
-            Juggernaut warrior2;
-            Wizard warrior3;
+            Console.WriteLine("Wybierz swoją klasę:");
+            Console.WriteLine("1. Warrior");
+            Console.WriteLine("2. Juggernaut");
+            Console.WriteLine("3. Wizard");
+            playerChoice = Console.ReadLine();
+            Hero player;
+            Hero opponent;
             do
             {
-                warrior1 = Warrior.GenerateRandomWarrior();
-                warrior2 = Juggernaut.GenerateRandomJuggernaut();
-                warrior3 = Wizard.GenerateRandomWizard();
-
-                if (new Random().Next(0,2) == 0)
+                if (playerChoice == "1")
                 {
-                    warrior2.Duel(warrior3);
+                    player = Warrior.GenerateRandomWarrior();
+                }
+                else if (playerChoice == "2")
+                {
+                    player = Juggernaut.GenerateRandomJuggernaut();
                 }
                 else
                 {
-                    warrior3.Duel(warrior2);
+                    player = Wizard.GenerateRandomWizard();
                 }
-                //warrior1.Duel(warrior2);
-                //warrior2.Duel(warrior3);
-                // ********** class test **********
-                if (warrior1.Wins == 1){ 
-                    win1++;
+                int opponentClass = new Random().Next(1, 4);
+                if (opponentClass == 1)
+                {
+                    opponent = Warrior.GenerateRandomWarrior();
                 }
-                else if(warrior2.Wins == 1){
-                    win2++;
+                else if (opponentClass == 2)
+                {
+                    opponent = Juggernaut.GenerateRandomJuggernaut();
                 }
-                else{
-                    win3++;
+                else
+                {
+                    opponent = Wizard.GenerateRandomWizard();
                 }
+                bool playerStarts = new Random().Next(0, 2) == 0;
+                if (playerStarts)
+                {
+                    Console.WriteLine("Gracz zaczyna pierwszy!");
+                    player.Duel(opponent);
+                }
+                else
+                {
+                    Console.WriteLine("Przeciwnik zaczyna pierwszy!");
+                    opponent.Duel(player);
+                }
+                // Zliczanie zwycięstw
+                if (player.Winner)
+                {
+                    winPlayer++;
+                }
+                else if (opponent.Winner)
+                {
+                    winOpponent++;
+                }
+
                 Console.WriteLine();
-                Console.WriteLine($"{warrior1.type}: {win1}");
-                Console.WriteLine($"{warrior2.type}: {win2}"); 
-                Console.WriteLine($"{warrior3.type}: {win3}");
+                Console.WriteLine($"{player.Type} (Gracz): {winPlayer} zwycięstw");
+                Console.WriteLine($"{opponent.Type} (Przeciwnik): {winOpponent} zwycięstw");
                 Console.WriteLine();
-            } while (win1 != goalwin && win2 != goalwin && win3 != goalwin);
+
+            } while (winPlayer < goalwin && winOpponent < goalwin);
             Console.ForegroundColor = ConsoleColor.Black;
         }
     }
 }
+
 
 //******* names bug fix **********
 //do
